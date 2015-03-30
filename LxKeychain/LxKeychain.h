03b5509e0c9ad6cfeeda3bbb9751e1d424fc95b9
lixian <lixian@git.etiantian.com>
2015-03-30 14:13:50 +0800

    check NSCoding protocol conforms

--- LxKeychain/LxKeychain.h
+++ LxKeychain/LxKeychain.h
@@ -3,6 +3,24 @@
 //  LxKeychain.h
 //
 
+/* -------------------- OSStatus probably value --------------------
+    errSecSuccess               = 0,       No error.
+    errSecUnimplemented         = -4,      Function or operation not implemented.
+    errSecIO                    = -36,     I/O error (bummers)
+    errSecOpWr                  = -49,     file already open with with write permission
+    errSecParam                 = -50,     One or more parameters passed to a function where not valid.
+    errSecAllocate              = -108,    Failed to allocate memory.
+    errSecUserCanceled          = -128,    User canceled the operation.
+    errSecBadReq                = -909,    Bad parameter or invalid state for operation.
+    errSecInternalComponent     = -2070,
+    errSecNotAvailable          = -25291,  No keychain is available. You may need to restart your computer.
+    errSecDuplicateItem         = -25299,  The specified item already exists in the keychain.
+    errSecItemNotFound          = -25300,  The specified item could not be found in the keychain.
+    errSecInteractionNotAllowed = -25308,  User interaction is not allowed.
+    errSecDecode                = -26275,  Unable to decode the provided data.
+    errSecAuthFailed            = -25293,  The user name or passphrase you entered is not correct.
+*/
+
 #import <Foundation/Foundation.h>
 #import <CommonCrypto/CommonDigest.h>
 
